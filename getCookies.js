b40536c8885b8bc0c1314573830d8504c71b23b3
Grey <381808327@qq.com>
2020-01-15 18:53:31 +0800

    Update getCookies.js

--- getCookies.js
+++ getCookies.js
@@ -2,4 +2,4 @@
 //var hasToken = $persistentStore.read('chavy_token_acfun');	
 var cookieVal = $request.headers['Cookie'];	
 var tokenVal = $request.headers['access_token'];	
-console.log(cookieVal+'---'+tokenVal);
+console.log(tokenVal);
