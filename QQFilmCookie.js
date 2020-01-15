db3dd318ea524555d871cccc75c75f10429d9f03
Grey <381808327@qq.com>
2020-01-15 20:13:32 +0800

    Update QQFilmCookie.js

--- QQFilmCookie.js
+++ QQFilmCookie.js
@@ -1,11 +1,9 @@
 var hasCookie = $persistentStore.read('chavy_cookie_videoqq');	
 var cookieVal = $request.headers['Cookie'];	
-if(cookieVal) {	
-   if(!hasCookie||(hasCookie!==cookieVal)) {	
+   if(hasCookie!==cookieVal) {	
       var setCookie = $persistentStore.write('chavy_cookie_videoqq', cookieVal);	
          if(setCookie) {	
            $notification.post('Result:', 'QQFilm', 'Success')	
          }	
     }	
-}	
 $done();
