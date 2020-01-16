c1046d6940f6cff654a40beb18219b9174d8455f
Grey <381808327@qq.com>
2020-01-16 17:13:59 +0800

    Update QQFilmCookie.js

--- QQFilmCookie.js
+++ QQFilmCookie.js
@@ -1,9 +1,22 @@
-var hasCookie = $persistentStore.read('chavy_cookie_videoqq');	
-var cookieVal = $request.headers['Cookie'];	
-   if(hasCookie!==cookieVal) {	
-      var setCookie = $persistentStore.write('chavy_cookie_videoqq', cookieVal);	
-         if(setCookie) {	
-           $notification.post('Result:', 'QQFilm', 'Success')	
-         }	
-    }	
+var CookieName = "腾讯视频";
+var CookieKey = "chavy_cookie_videoqq";
+var CookieValue = $request.headers['Cookie'];
+if ($persistentStore.read(CookieKey) != (undefined || null)) {
+  if ($persistentStore.read(CookieKey) != CookieValue) {
+    var cookie = $persistentStore.write(CookieValue, CookieKey);
+    if (!cookie) {
+      $notification.post("更新" + CookieName + "Cookie失败‼️", "", "");
+    } else {
+      $notification.post("更新" + CookieName + "Cookie成功 🎉", "", "");
+    }
+  }
+} else {
+  var cookie = $persistentStore.write(CookieValue, CookieKey);
+  if (!cookie) {
+    $notification.post("首次写入" + CookieName + "Cookie失败‼️", "", "");
+  } else {
+    $notification.post("首次写入" + CookieName + "Cookie成功 🎉", "", "");
+  }
+}
+
 $done();
