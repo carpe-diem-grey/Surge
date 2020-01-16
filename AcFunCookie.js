e8159954320287d76f0d3e3e3c48cd425dbe1be0
Grey <381808327@qq.com>
2020-01-16 17:04:35 +0800

    Update AcFunCookie.js

--- AcFunCookie.js
+++ AcFunCookie.js
@@ -1,22 +1,22 @@
-    var CookieName = "AcFun";
-    var CookieKey = "chavy_cookie_acfun";
-    var CookieValue = $request.headers['Cookie'];
-    if ($persistentStore.read(CookieKey) != (undefined || null)) {
-      if ($persistentStore.read(CookieKey) != CookieValue) {
-        var cookie = $persistentStore.write(CookieValue, CookieKey);
-        if (!cookie) {
-          $notification.post("更新" + CookieName + "Cookie失败‼️", "", "");
-        } else {
-          $notification.post("更新" + CookieName + "Cookie成功 🎉", "", "");
-        }
-      }
+var CookieName = "AcFun";
+var CookieKey = "chavy_cookie_acfun";
+var CookieValue = $request.headers['Cookie'];
+if ($persistentStore.read(CookieKey) != (undefined || null)) {
+  if ($persistentStore.read(CookieKey) != CookieValue) {
+    var cookie = $persistentStore.write(CookieValue, CookieKey);
+    if (!cookie) {
+      $notification.post("更新" + CookieName + "Cookie失败‼️", "", "");
     } else {
-      var cookie = $persistentStore.write(CookieValue, CookieKey);
-      if (!cookie) {
-        $notification.post("首次写入" + CookieName + "Cookie失败‼️", "", "");
-      } else {
-        $notification.post("首次写入" + CookieName + "Cookie成功 🎉", "", "");
-      }
+      $notification.post("更新" + CookieName + "Cookie成功 🎉", "", "");
     }
+  }
+} else {
+  var cookie = $persistentStore.write(CookieValue, CookieKey);
+  if (!cookie) {
+    $notification.post("首次写入" + CookieName + "Cookie失败‼️", "", "");
+  } else {
+    $notification.post("首次写入" + CookieName + "Cookie成功 🎉", "", "");
+  }
+}
 
 $done();
